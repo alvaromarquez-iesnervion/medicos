942f9ef305b068e477465e6159df1f2b7e0bc581
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-12-17 09:30:40 +0100

    si

--- db/client.py
+++ db/client.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
 
-db_client = MongoClient("mongodb://localhost:27017/")
+db_client = MongoClient("mongodb+srv://alvaro_marquez:12345@cluster0.eaeqbpz.mongodb.net/?appName=Cluster0")
+
